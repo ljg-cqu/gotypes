0e0933c8218a73ec64a9cb3254621859b0affb4a
ljg-cqu <ljg_cqu@126.com>
2022-03-28 13:19:32 +0800

    redefine Node and NodeTree types.

--- nodes.go
+++ nodes.go
@@ -2,16 +2,11 @@ package gotypes
 
 type Node struct {
 	Val  any
-	Next *Node
-}
-
-type NodePN struct {
-	Val  any
 	Prev *Node
 	Next *Node
 }
 
-type NodeLR struct {
+type NodeTree struct {
 	Val   any
 	Left  *Node
 	Right *Node
